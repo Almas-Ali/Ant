80d54de6a6eb219e52c4a15efe43dfc77e2412f7
Md. Almas Ali <almaspr3@gmail.com>
2022-10-08 22:07:55 +0600

    Added version management function.

--- src/userlib.py
+++ src/userlib.py
@@ -1,23 +1,31 @@
 class UserLib:
-	'''Ant library maker class.'''
-	
-	def __init__(self):
-		pass
-	
-	def __help__(self):
-		usege = '''Ant user library
+    '''Ant library maker class.'''
+
+    def __init__(self):
+        pass
+
+    def __help__(self):
+        usege = '''Ant user library
 version 1.0
 '''
-		print(usege)
-	
-	def run(self, args: list = None):
-		use = '''Congratulations!
+        print(usege)
+
+    def __version__(self):
+        ver = 'version 1.0'
+        print(ver)
+
+    def run(self, args: list = None):
+        use = '''Congratulations!
 Your library works!
 '''
-		if args[0] == 'help':
-			self.__help__()
-		else:
-			print(use)
+        if args[0] == 'help':
+            self.__help__()
+		
+        elif args[0] == '-v':
+            self.__version__()
 
-	def __repr__(self):
-		return '<Ant Library Object>'
+        else:
+            print(use)
+
+    def __repr__(self):
+        return '<Ant Library Object>'
