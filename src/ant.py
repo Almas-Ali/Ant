7f580bd0fdc7038353513a947d0bfe9bda51a121
Md. Almas Ali <almaspr3@gmail.com>
2022-12-27 02:45:08 +0600

    Added interactive CLI options.

--- src/ant.py
+++ src/ant.py
@@ -1,5 +1,6 @@
 import core
 from config import profile
+import argparse
 
 '''Ant - An Interpreter'''
 
@@ -11,4 +12,27 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--version', action='version',
+                        version=f'{core.__version__}')
+    parser.add_argument('-c', '--command', help='Run a command')
+    parser.add_argument('-s', '--script', help='Run a script')
+
+    args = parser.parse_args()
+
+    if args.command:
+        try:
+            _shell = core.Shell()
+            _shell.execute(args.command)
+        except Exception as e:
+            print(e)
+
+    elif args.script:
+        _shell = core.Shell()
+        with open(args.script, 'r') as _script:
+            for line in _script.readlines():
+                _shell.execute(line.strip())
+
+    else:
+        main()
