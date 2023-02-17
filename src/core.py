f6f301acbfb97a8f67a36a27c3e317de9cb99189
Md. Almas Ali <almaspr3@gmail.com>
2023-02-18 02:07:29 +0600

    Added internal commands support and path settings.

--- src/core.py
+++ src/core.py
@@ -1,12 +1,18 @@
-import readline
 import rlcompleter
 import atexit
 import os
 from importlib import import_module, reload
 import config
 import glob
+import sys
+import subprocess
+try:
+    import readline
+except:
+    from pyreadline3 import Readline
+    readline = Readline()
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 profile = config.profile
 
 
@@ -37,14 +43,39 @@ class Shell:
 
     def __init__(self, alias: dict = {}) -> None:
         self.vars: dict = {}
-        self.alias = alias
-        self.profile = profile
+        self.alias: dict = alias
+        self.profile: dict = profile
+        self.SYSTEM_PATH: list = []
+        # sys.path = []
+
+        # Is paths exists from path variables in config.py
+        # sys.path = []
+        for i in profile['ANT_PATH']:
+            if not os.path.exists(i):
+                print(f'Path {i} not found !')
+            else:
+                # self.ANT_PATH.append(i)
+                sys.path.insert(0, i)
+                # print(sys.path)
+
+        for i in profile['SYSTEM_PATH']:
+            if not os.path.exists(i):
+                print(f'Path {i} not found !')
+            else:
+                self.SYSTEM_PATH.append(i)
+
+        # Setting default variables
+        self.vars['HOME'] = profile['HOME_PATH']
+        self.vars['ANT_PATH'] = ', '.join(list(map(lambda x: f'\'{x}\'', profile['ANT_PATH'])))
+        self.vars['SYSTEM_PATH'] = ', '.join(list(map(lambda x: f'\'{x}\'', profile['SYSTEM_PATH'])))
+        self.vars['BASE_DIR'] = profile['BASE_DIR']
+        self.vars['ANT_VERSION'] = __version__
 
         # If history exists in path directory, Else create one.
         isHistoryExists = glob.glob(
-            os.path.join(profile['path'], profile['history']))
+            os.path.join(profile['HOME_PATH'], profile['history']))
         if isHistoryExists == []:
-            with open(os.path.join(profile['path'], profile['history']), 'a') as history:
+            with open(os.path.join(profile['HOME_PATH'], profile['history']), 'a') as history:
                 history.write('')
 
     def __repr__(self):
@@ -62,19 +93,20 @@ class Shell:
                 reload(config)
                 profile = config.profile
 
-                input_ = input(profile['prompt'])
+                input_ = input(profile['PROMPT'])
                 self.execute(input_)
                 self.store_history(input_)
-
+                print()
+                
             except KeyboardInterrupt:
                 # print('\nKeyboard interupt ! Type exit to exit.')
                 print()
 
     def shell_history(self):
 
-        commands = glob.glob(os.path.join(profile['root'], 'bin/*.py'))
+        commands = glob.glob(os.path.join(profile['BASE_DIR'], 'bin/*.py'))
         commands = [x.replace(os.path.join(
-            profile['root'], 'bin/'), '').replace('.py', '') for x in commands]
+            profile['BASE_DIR'], 'bin/'), '').replace('.py', '') for x in commands]
 
         readline.set_completer(HistoryManager(commands).completer1)
         # readline.set_completer(completer2)
@@ -85,7 +117,7 @@ class Shell:
         else:
             readline.parse_and_bind("tab: complete")
 
-        histfile = os.path.join(profile['path'], profile['history'])
+        histfile = os.path.join(profile['HOME_PATH'], profile['history'])
 
         try:
             readline.read_history_file(histfile)
@@ -98,7 +130,7 @@ class Shell:
         # HistoryManager.store_history(histfile)
 
     def store_history(self, command: str):
-        with open(os.path.join(profile['path'], profile['history']), 'a') as history:
+        with open(os.path.join(profile['HOME_PATH'], profile['history']), 'a') as history:
             history.write(f'{command}\n')
 
     def script_executer(self, script: str):
@@ -191,27 +223,47 @@ class Shell:
                 data = [j.strip() for j in data]
                 self.alias[data[0]] = data[1]
             except:
+                print('Aliases: ')
+                print('-'*30)
                 for key, value in self.alias.items():
-                    print(f'{key} = {value}')
+                    print(f'\'{key}\' \b\b\b\t\t-> \'{value}\'')
 
         else:
+            # Try to call commands from self.ANT_PATH else from aliases else from system commands from self.SYSTEM_PATH else throw error !
             try:
-                reload(import_module(f'bin.{input_[0]}'))
-                file = getattr(import_module(
-                    f'bin.{input_[0]}'), 'Exclusive')
-                file = file()
-
-                arg = input_[1:]
-                # Manage empty args
-                if arg == []:
-                    arg.append('')
-
-                file.run(args=arg, vars=self.vars,)
-
-            except ImportError:
                 try:
-                    val = self.alias[input_[0]]
-                    val = f"{val} {' '.join(input_[1:])}"
-                    self.execute(val)
+                    try:
+                        # get modules from python path and reload them to get the latest changes
+                        file = import_module(f'{input_[0]}')
+                        reload(file)
+                        file = getattr(file, 'Exclusive')
+                        file = file()
+
+                        arg = input_[1:]
+                        # Manage empty args
+                        if arg == []:
+                            arg.append('')
+
+                        file.run(args=arg, vars=self.vars, profile=self.profile)
+
+                    except:
+                        val = self.alias[input_[0]]
+                        val = f"{val} {' '.join(input_[1:])}"
+                        self.execute(val)
+
                 except:
-                    print(f'ant: \"{input_[0]}\" not found!')
+                    # If not found in self.ANT_PATH, try to call system commands from self.SYSTEM_PATH
+                    for h, i in enumerate(self.SYSTEM_PATH):
+                        success, fail = subprocess.Popen(
+                            f'{i}/{" ".join(input_)}', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+
+                        if success.decode('utf-8') != '':
+                            print(success.decode('utf-8'))
+                            break
+
+                        if len(self.SYSTEM_PATH) - 1 == h:
+                            break
+
+            except Exception as e:
+                print(e)
+                print(f'ant: \"{input_[0]}\" not found!')
