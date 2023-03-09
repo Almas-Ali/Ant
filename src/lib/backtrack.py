aa87c563a4894dd8507431b624918ec206ddb84d
Md. Almas Ali <almaspr3@gmail.com>
2023-03-10 00:53:19 +0600

    Added new error types.

--- src/lib/backtrack.py
+++ src/lib/backtrack.py
@@ -1,24 +1,34 @@
+from lib.userlib import stdlib
+
 
 class Errors:
     '''Ant errors class.'''
 
     def __init__(self) -> None:
-        pass
+        self.output = stdlib().error
 
     def command_not_found(self, command: str) -> None:
-        print(f'Ant: {command}: command not found')
+        self.output(f'Ant: {command}: command not found!')
 
     def command_not_found_help(self, command: str) -> None:
-        print(
+        self.output(
             f'Ant: {command}: command not found. Try "help" for more information.'
         )
 
     def error(self, error: str) -> None:
-        print(f'Ant: {error}')
+        self.output(f'Ant: {error} error!')
 
     def error_help(self, error: str) -> None:
-        print(f'Ant: {error}. Try "help" for more information.')
+        self.output(f'Ant: {error}. Try "help" for more information.')
 
     def syntax_error(self, command: str) -> None:
-        print(f'Ant: Syntax error')
+        self.output(f'Ant: {command}: syntax error!')
 
+    def undefined_error(self, command: str) -> None:
+        self.output(f'Ant: \'${command}\' undefined!')
+    
+    def file_not_found(self, command: str, filename: str) -> None:
+        self.output(f'Ant: {command}: \"{filename}\": No such file or directory!')
+
+    def custom_error(self, command: str, text: str) -> None:
+        self.output(f'Ant: {command}: {text}')
